4e0ae06682eb7d43f981aad31b0c5f1f0e144c2f
Softwarica College Lab <softwarica@softwarica.com>
2019-05-12 12:06:55 +0545

    Done

--- app/src/main/java/com/example/retrofit/MainActivity.java
+++ app/src/main/java/com/example/retrofit/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.retrofit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,7 +26,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tvdata=findViewById(R.id.tvdata);
-
 
         Retrofit retrofit=new Retrofit.Builder()
                             .baseUrl("http://dummy.restapiexample.com/api/v1/")
